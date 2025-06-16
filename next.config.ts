0c573f8e63b911c839b23e3a707affc88c070fa7
Andrii Stepaniuk <qfrost@MacBook-Air-Andrii.local>
2025-06-16 15:17:23 +0200

    added configutation for github pages

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
-  // basePath: "./",
-  // images: {
-  //   loader: 'custom',
-  //   loaderFile: './loader.ts',
-  // },
+  basePath: "/andrii-portfolio",
+  images: {
+    loader: 'custom',
+    loaderFile: './loader.ts',
+  },
 };
 
 export default nextConfig;
