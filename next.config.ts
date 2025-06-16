234157bc1b0528f54fef985d839436801f063199
Andrii Stepaniuk <qfrost@MacBook-Air-Andrii.local>
2025-06-16 14:57:44 +0200

    add deploy configuration

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  // basePath: "./",
+  images: {
+    loader: 'custom',
+    loaderFile: './loader.ts',
+  },
 };
 
 export default nextConfig;
